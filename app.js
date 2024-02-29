b5d5bb9b423b4f62739ebe40a31a30ebab8ba6c7
Muhammad Faizan <fiazansuri855@gmail.com>
2024-02-29 11:27:46 +0500

    Js-if-else-assignment

---
+++ app.js
@@ -0,0 +1,43 @@
+// console.log("hello world");
+
+var percentage = +prompt("Enter your Percentage");
+
+if(percentage > 100){
+    console.log("Bhai percentage sahi dal de");
+} else if(percentage >= 50 && percentage <= 60){
+    console.log("C grade aya hai");
+} else if(percentage >= 61 && percentage <= 70){
+    console.log("B grade aya hai");
+} else if(percentage >= 71 && percentage <= 80){
+    console.log("A grade aya hai");
+} else if(percentage >= 81 && percentage <= 90){
+    console.log("A-one Grade aya hai");
+} else if(percentage >= 91 && percentage <= 100){
+    console.log("A+one grade aya hai");
+}
+else{(percentage >= 50)
+    console.log('Bhai ap fail hogaye ho');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
